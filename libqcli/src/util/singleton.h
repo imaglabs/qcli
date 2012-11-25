a9da553f5aa5e547feeadf73e184184f0b0c031d
Pablo Odorico <pablo.odorico@gmail.com>
2012-11-25 15:04:13 -0300

    singleton license/copyright notice

--- libqcli/src/util/singleton.h
+++ libqcli/src/util/singleton.h
@@ -1,8 +1,22 @@
+/*
+ *    Copyright (C) 2012 libqcli authors (see AUTHORS)
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ */
+
 #ifndef _QCLI_SINGLETON_H
 #define _QCLI_SINGLETON_H
 
-/// \brief Singleton pattern template
-/// @param T should provide a default constructor.
+/// \brief Singleton pattern template.
+/// @param T should provide a public default constructor.
 
 template<class T>
 class Singleton
