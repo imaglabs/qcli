27c484bc9cdf20fdd4ff94e201cf599f71e8d12b
Pablo Odorico <pablo.odorico@gmail.com>
2012-11-25 18:30:12 -0300

    more api ideas

--- libqcli/api_ideas.cpp
+++ libqcli/api_ideas.cpp
@@ -23,16 +23,35 @@ kernel(input, output);
 QCLI::Kernel::perPixel("y= 2.0f * x").run(input, output);
 
 // Kernel as a variadic template for the kernel parameters?
-auto kernel= QCLI::Kernel<2>(R"
+auto kernel= QCLI::Kernel<2>(R"(
     ...
-    ");
+    )");
 kernel(param1, param2);
 
 // maybe make Kernel a variadic template and pass all argtypes as template params?
-auto kernel= QCLI::Kernel<int, QCL::Image, QSize>(R("
-    ...
-    "));
+auto kernel= QCLI::Kernel<QCL::Image, QCL::Image>(R"(
+    full kernel definition, including header
+    )");
 
+// Other idea
+// See if it is possible to pass (class T, string N) * n template params, else use some sort of QPair o "KArg<class T, string N>"
+// Texture will be some sort of typdef to define that that parameter is __read_only
+auto kernel= Kernel<Texture, "input", Image, "output">(R"(
+    // Only type the kernel body
+    const sampler_t sampler= CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_BILINEAR;
+    float3 value= read_imagef(input, sampler, (int2)(x  , y-1)).xyz;
+    write_imagef(output, (int2)(x,y), value);
+)");
+
+The kernel header can be auto-generated:
+    __kernel void fdmHeat(
+        __read_only image2d_t input,
+        __write_only image2d_t output)
+    {
+
+// The template parameters can be used to extract static type information, and to define a single run/operator() function
+kernel(Texture, Image);
+        
 
 // Loads image from a QImage and changes all the parameters (size, remalloc, etc.) as needed
 // if neccessary discard the pimpl and create a new one with the new params
